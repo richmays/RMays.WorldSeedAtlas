9ee86704bc4a53a5e63de32e5c6e5bea2953b465
Rich Mays <richmays+github@gmail.com>
2020-04-29 00:38:28 -0400

    Added more UI elements.  TODO: Bind the CheckListBox to a data table.

--- RMays.WorldSeedAtlas.UI/Form1.cs
+++ RMays.WorldSeedAtlas.UI/Form1.cs
@@ -19,16 +19,94 @@ namespace RMays.WorldSeedAtlas.UI
 
         protected LocationList locationList;
 
+        private string CurrentPosition
+        {
+            get
+            {
+                return txtStartingPosition.Text;
+            }
+            set
+            {
+                txtStartingPosition.Text = value;
+            }
+        }
+
         private void txtStartingPosition_TextChanged(object sender, EventArgs e)
         {
             // Recalculate items in the list.
-
-
+            if (!IsValid(CurrentPosition)) return;
+            UpdateLocations();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             locationList = new LocationList("locations.json");
+            UpdateLocations();
+        }
+
+        private void UpdateLocations()
+        {
+            clbLocations.Items.Clear();
+            var LocationsToAdd = new List<SmartLocation>();
+            foreach(var item in locationList)
+            {
+                if (!item.Visible)
+                {
+                    continue;
+                }
+
+                if (item.Checked && this.chkHideCheckedLocations.Checked)
+                {
+                    continue;
+                }
+
+                item.RecalculateDistance(CurrentPosition);
+                LocationsToAdd.Add(item);
+            }
+
+            LocationsToAdd.Sort();
+            foreach(var location in LocationsToAdd)
+            {
+                clbLocations.Items.Add(location.ToString(), location.Checked);
+            }
+        }
+
+        private bool IsValid(string currentLocation)
+        {
+            if (string.IsNullOrWhiteSpace(currentLocation))
+            {
+                return false;
+            }
+
+            if (currentLocation.Length < 2 || currentLocation.Length > 3)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private void clbLocations_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            /*
+            if (!chkHideCheckedLocations.Checked) return;
+
+            for (var i = 0; i < clbLocations.Items.Count; i++)
+            {
+                locationList.ElementAt(i).Visible = true;
+                locationList.ElementAt(i).Checked = clbLocations.CheckedItems.Contains(locationList.ElementAt(i).ToString());
+            }
+            */
+
+            /*
+            foreach(var item in clbLocations.CheckedItems)
+            {
+                var location = locationList.Where(x => x.ToString() == item.ToString()).FirstOrDefault();
+                location.Checked = true;
+                location.Visible = false;
+            }
+            */
         }
     }
 }
+
--- RMays.WorldSeedAtlas/Location.cs
+++ RMays.WorldSeedAtlas/Location.cs
@@ -7,12 +7,29 @@ using System.Threading.Tasks;
 namespace RMays.WorldSeedAtlas
 {
     [Serializable]
-    public class Location
+    public class Location : IComparable<Location>
     {
         public string Name { get; set; }
         public string BossName { get; set; }
         public string Type { get; set; }
         public string XCoord { get; set; }
         public string YCoord { get; set; }
+
+        public virtual int CompareTo(Location other)
+        {
+            return this.ToString().CompareTo(other.ToString());
+        }
+
+        public override string ToString()
+        {
+            if (string.IsNullOrWhiteSpace(this.BossName))
+            {
+                return $"{Name} - {XCoord}{YCoord}";
+            }
+            else
+            {
+                return $"{Name} ({BossName}) - {XCoord}{YCoord}";
+            }
+        }
     }
 }
